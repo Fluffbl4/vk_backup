585465513845098fc659c6f280de401382b73a80
Sergey Karyukhin <prajs601@yandex.ru>
2024-11-23 10:59:49 +0300

    Changed vk_backup.py the commented-out code has been deleted

--- vk_backup.py
+++ vk_backup.py
@@ -74,30 +74,6 @@ class PhotoDownloader:
             })
 
         return max_size_photos[:self.count]
-    # def get_max_size_photos(self):
-    #     try:
-    #         photos = self.session.photos.get(
-    #             owner_id=self.user_id, album_id="profile", extended=1
-    #         )["items"]
-    #     except Exception as e:
-    #         print(f"Произошла ошибка при получении фотографий: {e}")
-    #         sys.exit(1)
-    #
-    #     max_size_photos = []
-    #     for photo in photos:
-    #         sizes = sorted(
-    #             photo["sizes"], key=lambda x: x["width"] * x["height"], reverse=True
-    #         )
-    #         max_size_photo = sizes[0]
-    #         max_size_photos.append(
-    #             {
-    #                 "url": max_size_photo["url"],
-    #                 "likes_count": photo["likes"]["count"],
-    #                 "id": photo["id"],
-    #             }
-    #         )
-    #
-    #     return max_size_photos[: self.count]
 
     def download_and_upload_to_ya_disk(self, folder_name):
         if not self.check_folder_exists_on_ya_disk(folder_name):
@@ -140,42 +116,6 @@ class PhotoDownloader:
             self.progress_bar.update(1)
 
         return results
-    # def download_and_upload_to_ya_disk(self, folder_name):
-    #     if not self.check_folder_exists_on_ya_disk(folder_name):
-    #         self.create_folder_on_ya_disk(folder_name)
-    #
-    #     existing_files = self.get_existing_files_in_folder(folder_name)
-    #     max_size_photos = self.get_max_size_photos()
-    #
-    #     results = []
-    #     self.progress_bar = tqdm.tqdm(total=len(max_size_photos), desc="Загрузка")
-    #     for photo in max_size_photos:
-    #         filename = f"{folder_name}/{photo['likes_count']}.jpg"
-    #         if f"{photo['likes_count']}.jpg" not in existing_files:
-    #             response = requests.get(photo["url"])
-    #             if response.status_code == 200:
-    #                 file_data = response.content
-    #                 success = self.upload_to_ya_disk(file_data, filename)
-    #
-    #                 if success:
-    #                     results.append(
-    #                         {
-    #                             "filename": filename,
-    #                             "likes_count": photo["likes_count"],
-    #                             "photo_id": photo["id"],
-    #                         }
-    #                     )
-    #                     print(f"Фото успешно сохранено: {filename}")
-    #                 else:
-    #                     print(f"Не удалось сохранить фото: {filename}")
-    #             else:
-    #                 print(f"Не удалось скачать фото: {photo['url']}")
-    #         else:
-    #             print(f"Фото {filename} уже существует в папке.")
-    #
-    #         self.progress_bar.update(1)
-    #
-    #     return results
 
     def auth_to_ya_disk(self, ya_disk_token):
         self.ya_disk_token = ya_disk_token
