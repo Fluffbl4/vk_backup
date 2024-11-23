d8f0ec554252f5a319c6be91a009beb9ab3af60e
Sergey Karyukhin <prajs601@yandex.ru>
2024-11-23 10:37:25 +0300

    Changed vk_backup.py

--- vk_backup.py
+++ vk_backup.py
@@ -4,6 +4,8 @@ import requests
 import json
 import tqdm
 from vk_api import VkApi
+from PIL import Image
+import io
 
 
 class PhotoDownloader:
@@ -54,28 +56,48 @@ class PhotoDownloader:
 
     def get_max_size_photos(self):
         try:
-            photos = self.session.photos.get(
-                owner_id=self.user_id, album_id="profile", extended=1
-            )["items"]
+            photos = self.session.photos.get(owner_id=self.user_id, album_id="profile", extended=1)["items"]
         except Exception as e:
             print(f"Произошла ошибка при получении фотографий: {e}")
             sys.exit(1)
 
         max_size_photos = []
         for photo in photos:
-            sizes = sorted(
-                photo["sizes"], key=lambda x: x["width"] * x["height"], reverse=True
-            )
+            sizes = sorted(photo["sizes"], key=lambda x: x["width"] * x["height"], reverse=True)
             max_size_photo = sizes[0]
-            max_size_photos.append(
-                {
-                    "url": max_size_photo["url"],
-                    "likes_count": photo["likes"]["count"],
-                    "id": photo["id"],
-                }
-            )
+            max_size_photos.append({
+                "url": max_size_photo["url"],
+                "width": max_size_photo["width"],
+                "height": max_size_photo["height"],
+                "likes_count": photo["likes"]["count"],
+                "id": photo["id"]
+            })
 
-        return max_size_photos[: self.count]
+        return max_size_photos[:self.count]
+    # def get_max_size_photos(self):
+    #     try:
+    #         photos = self.session.photos.get(
+    #             owner_id=self.user_id, album_id="profile", extended=1
+    #         )["items"]
+    #     except Exception as e:
+    #         print(f"Произошла ошибка при получении фотографий: {e}")
+    #         sys.exit(1)
+    #
+    #     max_size_photos = []
+    #     for photo in photos:
+    #         sizes = sorted(
+    #             photo["sizes"], key=lambda x: x["width"] * x["height"], reverse=True
+    #         )
+    #         max_size_photo = sizes[0]
+    #         max_size_photos.append(
+    #             {
+    #                 "url": max_size_photo["url"],
+    #                 "likes_count": photo["likes"]["count"],
+    #                 "id": photo["id"],
+    #             }
+    #         )
+    #
+    #     return max_size_photos[: self.count]
 
     def download_and_upload_to_ya_disk(self, folder_name):
         if not self.check_folder_exists_on_ya_disk(folder_name):
@@ -91,13 +113,18 @@ class PhotoDownloader:
             if f"{photo['likes_count']}.jpg" not in existing_files:
                 response = requests.get(photo["url"])
                 if response.status_code == 200:
-                    file_data = response.content
-                    success = self.upload_to_ya_disk(file_data, filename)
+                    image_data = response.content
+                    with Image.open(io.BytesIO(image_data)) as img:
+                        width, height = img.size
+
+                    success = self.upload_to_ya_disk(image_data, filename)
 
                     if success:
                         results.append(
                             {
                                 "filename": filename,
+                                "width": width,
+                                "height": height,
                                 "likes_count": photo["likes_count"],
                                 "photo_id": photo["id"],
                             }
@@ -113,18 +140,62 @@ class PhotoDownloader:
             self.progress_bar.update(1)
 
         return results
+    # def download_and_upload_to_ya_disk(self, folder_name):
+    #     if not self.check_folder_exists_on_ya_disk(folder_name):
+    #         self.create_folder_on_ya_disk(folder_name)
+    #
+    #     existing_files = self.get_existing_files_in_folder(folder_name)
+    #     max_size_photos = self.get_max_size_photos()
+    #
+    #     results = []
+    #     self.progress_bar = tqdm.tqdm(total=len(max_size_photos), desc="Загрузка")
+    #     for photo in max_size_photos:
+    #         filename = f"{folder_name}/{photo['likes_count']}.jpg"
+    #         if f"{photo['likes_count']}.jpg" not in existing_files:
+    #             response = requests.get(photo["url"])
+    #             if response.status_code == 200:
+    #                 file_data = response.content
+    #                 success = self.upload_to_ya_disk(file_data, filename)
+    #
+    #                 if success:
+    #                     results.append(
+    #                         {
+    #                             "filename": filename,
+    #                             "likes_count": photo["likes_count"],
+    #                             "photo_id": photo["id"],
+    #                         }
+    #                     )
+    #                     print(f"Фото успешно сохранено: {filename}")
+    #                 else:
+    #                     print(f"Не удалось сохранить фото: {filename}")
+    #             else:
+    #                 print(f"Не удалось скачать фото: {photo['url']}")
+    #         else:
+    #             print(f"Фото {filename} уже существует в папке.")
+    #
+    #         self.progress_bar.update(1)
+    #
+    #     return results
+
+    def auth_to_ya_disk(self, ya_disk_token):
+        self.ya_disk_token = ya_disk_token
 
     def upload_to_ya_disk(self, file_data, filename):
-        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        headers = {"Authorization": f"OAuth {self.ya_disk_token}"}
-        params = {"path": filename, "overwrite": True}
-        response = requests.post(url, headers=headers, params=params)
-        if response.status_code == 200 or response.status_code == 201:
-            upload_url = response.json()["href"]
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = {'Authorization': f'OAuth {self.ya_disk_token}'}
+        params = {'path': filename, 'overwrite': 'true'}
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            upload_url = response.json()['href']
             put_response = requests.put(upload_url, data=file_data)
-            return put_response.status_code == 201
+            if put_response.status_code == 201:
+                return True
+            else:
+                print(f'Ошибка при загрузке файла: {put_response.text}')
+                return False
         else:
-            print(f"Ошибка при получении URL для загрузки файла: {response.text}")
+            print(f'Ошибка при получении URL для загрузки файла: {response.text}')
             return False
 
     def save_results_to_json(self, results, output_file="photos_info.json"):
